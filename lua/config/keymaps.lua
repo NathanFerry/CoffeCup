040ac77ea89362b90ea683326d399e3a983344fa
Nathan FERRY <blackdragon14700@gmail.com>
2024-08-13 17:30:19 +0200

    Adding Telescope and TreeSitter

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,7 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>w', '<cmd>w<cr>')
 vim.keymap.set('n', '<leader>q', '<cmd>q<cr>')
 vim.keymap.set('n', '<leader>e', '<cmd>Explore<cr>')
+
+-- Telescope
+local telescope = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope.find_files, {desc = 'Find a file'})

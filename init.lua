ed83d3953a1290a436fad577e14823e587d93453
Nathan FERRY <blackdragon14700@gmail.com>
2024-08-14 09:02:56 +0200

    Fixing import order

--- init.lua
+++ init.lua
@@ -1,5 +1,9 @@
-require('config.keymaps')
 require('config.lazy')
+require('config.keymaps')
 
 -- Default colorscheme
 vim.cmd([[colorscheme catppuccin-frappe]])
+
+-- https://medium.com/@shaikzahid0713/code-completion-for-neovim-6127401ebec2
+-- Yeah, this is a link to help me with a list of plugins to add...
+-- What you gonna do ? Huh.

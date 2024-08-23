219663e2826a2a561f4be1c9c062ecea1939a5bf
Nathan Ferry <nathan.ferry@live.fr>
2024-08-23 13:03:00 +0200

    Added keymap for window splitting and buffer navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,26 @@ vim.keymap.set("n", "<leader>e", "<cmd>Explore<cr>")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
+-- Tab on visual mode
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv")
+
+-- Split window
+vim.keymap.set("n", "ss", "<cmd>split<CR>", { desc = "Split screen horizontaly" })
+vim.keymap.set("n", "sv", "<cmd>vsplit<CR>", { desc = "Split screen verticaly" })
+
+-- Move between buffers
+vim.keymap.set("n", "sh", "<C-w>h", { desc = "Move to left buffer" })
+vim.keymap.set("n", "sj", "<C-w>j", { desc = "Move to below buffer" })
+vim.keymap.set("n", "sk", "<C-w>k", { desc = "Move to above buffer" })
+vim.keymap.set("n", "sl", "<C-w>l", { desc = "Move to right buffer" })
+
+-- Resize buffer
+vim.keymap.set("n", "<C-S-h>", "<C-w><")
+vim.keymap.set("n", "<C-S-j>", "<C-w>-")
+vim.keymap.set("n", "<C-S-k>", "<C-w>+")
+vim.keymap.set("n", "<C-S-l>", "<C-w>>")
+
 -- Telescope
 local telescope = require("telescope.builtin")
 vim.keymap.set("n", "<leader>sf", telescope.find_files, { desc = "Find a file" })
@@ -17,7 +37,3 @@ vim.keymap.set("n", "<leader>lm", "<cmd>Mason<CR>", { desc = "Display Mason menu
 
 -- Lazy
 vim.keymap.set("n", "<leader>m", "<cmd>Lazy<CR>", { desc = "Display Lazy menu" })
-
--- Tab on visual mode
-vim.keymap.set("v", "<Tab>", ">gv")
-vim.keymap.set("v", "<S-Tab>", "<gv")
